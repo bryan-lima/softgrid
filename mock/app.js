88f214ddcbbe590a72ca4c72591b417bf146b8b5
afpop <jpcontato988@gmail.com>
2017-12-06 14:04:41 -0200

    Correção

--- mock/app.js
+++ mock/app.js
@@ -7,19 +7,21 @@ angular.module('todoApp', ['softgrid.directive','ngSanitize'])
 
         function _gerarGrid() {
 
-            vm.colunas = [
-                {title: "Nome",     item: function(item){return item.nome}, edit: { item: "nome", function: _editar, width: "100%"}},
-                {title: "Cargo",    item: function(item){return item.cargo}, align: "center"},
-                {title: "Telefone", item: function(item){return item.contato.telefone}}
-            ];
-
-            vm.acoes = [
-                {title: "Editar",   icon: "fa fa-info-circle", function: _teste},
-                {title: "Excluir",  icon: "fa fa-wrench",      function: _teste},
-                {title: "Bloquear", icon: "fa fa-calculator",  function: _teste}
-            ];
-
-            vm.data = [
+            var _sub = [
+                {nome: "Opa", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: true},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: true},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
                 {nome: "Opa", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
                 {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false},
                 {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false},
@@ -37,6 +39,24 @@ angular.module('todoApp', ['softgrid.directive','ngSanitize'])
                 {nome: "Peixoto", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0404"}, ativo: false}
             ];
 
+            vm.colunas = [
+                {title: "Nome",     item: function(item){return item.nome}, edit: { item: "nome", function: _editar, width: "100%"}},
+                {title: "Cargo",    item: function(item){return item.cargo}, align: "center"},
+                {title: "Telefone", item: function(item){return item.contato.telefone}}
+            ];
+
+            vm.acoes = [
+                {title: "Editar",   icon: "fa fa-info-circle", function: _teste},
+                {title: "Excluir",  icon: "fa fa-wrench",      function: _teste},
+                {title: "Bloquear", icon: "fa fa-calculator",  function: _teste}
+            ];
+
+            vm.data = [
+                {nome: "Opa", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false, sub: _sub},
+                {nome: "Nunes", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0303"}, ativo: false, sub: _sub},
+                {nome: "Jack", cargo: "Programador", contato: {tipo: "residencial", telefone: "12 90101-0202"}, ativo: false, sub: _sub}
+            ];
+
             vm.menu = [{title: "Cadastrar", icon: "fa fa-plus", function: _teste}];
 
             vm.controles = {};
@@ -46,6 +66,16 @@ angular.module('todoApp', ['softgrid.directive','ngSanitize'])
             vm.controles.activeTitle = "Ativar/Desativar";
             vm.controles.activeCol = function(item){return item.ativo};
             vm.controles.activeFunction = _teste;
+
+            vm.subgrid = {};
+            vm.subgrid.object = "sub";
+            vm.subgrid.cols = [
+                {title: "Nome",     item: function(item){return item.nome}, edit: { item: "nome", function: _editar, width: "100%"}},
+                {title: "Cargo",    item: function(item){return item.cargo}, align: "center"},
+                {title: "Telefone", item: function(item){return item.contato.telefone}}
+            ];
+            vm.subgrid.hide = {all:true};
+
         }
 
         function _editar(item){
